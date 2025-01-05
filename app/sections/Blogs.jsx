e92f3db306ff255037eeb73e91514929010dff6b
Rumaisa Naveed <rumaisanaved@gmail.com>
2025-01-05 18:09:13 +0500

    feat: Added the blogs fetching code

--- app/sections/Blogs.jsx
+++ app/sections/Blogs.jsx
@@ -4,8 +4,53 @@ import React from "react";
 import Calendar from "../../public/icons/greenCalendar.svg";
 import Heading from "../components/ui/Heading";
 
-export const Blogs = ({ data }) => {
-  console.log(data);
+export const Blogs = ({ articles }) => {
+  console.log(articles);
+  // const getArticles = async () => {
+  //   const res = await fetch("https://gql.hashnode.com/", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       query: `query getArticles {
+  //         publication(host: "rumaisanaveed.hashnode.dev") {
+  //           title
+  //           posts(first: 2) {
+  //             edges {
+  //               node {
+  //                 title
+  //                 publishedAt
+  //                 subtitle
+  //                 url
+  //               }
+  //             }
+  //           }
+  //         }
+  //       }`,
+  //       variables: {},
+  //     }),
+  //   });
+
+  //   if (!res.ok) {
+  //     console.log("Failed to fetch articles");
+  //     return;
+  //   }
+
+  //   const { data } = await res.json();
+  //   console.log(data);
+  //   const {
+  //     publication: {
+  //       posts: { edges: articles },
+  //     },
+  //   } = data;
+  //   console.log(articles);
+  // };
+
+  // useEffect(() => {
+  //   getArticles();
+  // }, []);
+
   return (
     <div className="flex flex-col gap-5">
       <Heading title="Blogs" />
@@ -44,45 +89,48 @@ export const Blogs = ({ data }) => {
 };
 
 export default async function getServerSideProps() {
-  const res = await fetch("https://api.hashnode.com", {
+  const res = await fetch("https://gql.hashnode.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      query: `
-        query {
-              publication(host: "rumaisanaveed.hashnode.dev") {
-              title
-              posts(first: 2) {
-                edges {
-                  node {
-                    title
-                    publishedAt
-                    subtitle
-                    url
-                  }
+      query: `query getArticles {
+        publication(host: "rumaisanaveed.hashnode.dev") {
+          title
+          posts(first: 2) {
+            edges {
+              node {
+                title
+                publishedAt
+                subtitle
+                url
               }
             }
-         }
-     }`,
+          }
+        }
+      }`,
+      variables: {},
     }),
   });
 
   if (!res.ok) {
-    console.error("Failed to fetch articles:", await res.text());
+    console.log("Failed to fetch articles");
+    return;
   }
 
   const { data } = await res.json();
 
-  if (data) {
-    const {
-      publication: {
-        posts: { edges: articles },
-      },
-    } = data;
-    return {
-      props: { articles },
-    };
-  }
+  const {
+    publication: {
+      posts: { edges: articles },
+    },
+  } = data;
+  console.log(articles);
+
+  return {
+    props: {
+      articles,
+    },
+  };
 }
